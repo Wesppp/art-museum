9adeed4a08efbbc8fa8ce43625fa42f3c4bceb9e
wesppp <mshesnovich@mail.ru>
2024-04-20 10:15:28 +0300

    made a blank for the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from '@modules/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
