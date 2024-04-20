a8f6bc6f41b5279f451ea061ad37cabf467ad6ce
wesppp <mshesnovich@mail.ru>
2024-04-20 13:05:54 +0300

    ceate loading service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '@components/header/header.component';
 import { FooterComponent } from '@components/footer/footer.component';
-import { ArtworksService } from '@services/artworks.service';
-import { Observable } from 'rxjs';
-import { GetArtworksResponse } from '@models/get-artworks-response.interface';
-import { CommonModule } from '@angular/common';
-import { ArtworkCardComponent } from '@components/artwork-card/artwork-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent,
-    CommonModule,
-    ArtworkCardComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  title = 'art-museum';
-  public artworks$!: Observable<GetArtworksResponse>;
-
-  constructor(private readonly artworksService: ArtworksService) {}
-
-  ngOnInit(): void {
-    this.artworks$ = this.artworksService.getArtworks();
-  }
+export class AppComponent {
+  public title: string = 'art-museum';
 }
--- src/app/services/artworks.service.ts
+++ src/app/services/artworks.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, finalize, tap } from 'rxjs';
 
 import { environment } from '@environments/environment.development';
 import { GetArtworksResponse } from '@models/get-artworks-response.interface';
+import { LoadingsService } from './loadings.service';
+import { Loadings } from '@enums/loadings.enum';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArtworksService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly loadingService: LoadingsService
+  ) {}
 
   public getArtworks(): Observable<GetArtworksResponse> {
-    return this.http.get<GetArtworksResponse>(`${environment.apiUrl}/artworks`);
+    return this.http
+      .get<GetArtworksResponse>(`${environment.apiUrl}/artworks`)
+      .pipe(
+        tap(() => this.loadingService.addLoading(Loadings.ALL_ARTWORKS)),
+        finalize(() => this.loadingService.removeLoading(Loadings.ALL_ARTWORKS))
+      );
   }
 }
