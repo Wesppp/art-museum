121f273e0e89f884ac404b1d43caae5ab8da73d4
wesppp <mshesnovich@mail.ru>
2024-04-22 23:30:18 +0300

    add error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '@components/header/header.component';
 import { FooterComponent } from '@components/footer/footer.component';
+import { ToastMessageComponent } from '@components/toast-message/toast-message.component';
+import { MessageService } from '@services/message.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,25 @@ import { FooterComponent } from '@components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('messages', { read: ViewContainerRef })
+  messagesContainer!: ViewContainerRef;
+
   public title: string = 'art-museum';
+
+  constructor(private messageService: MessageService) {}
+
+  public ngOnInit(): void {
+    this.messageService.errorMessage$.subscribe((message: string) => {
+      this.createComponent(message);
+    });
+  }
+
+  public createComponent(message: string): void {
+    const messageRef = this.messagesContainer.createComponent(
+      ToastMessageComponent
+    );
+
+    messageRef.setInput('message', message);
+  }
 }
